8262a97c6f21bedae282b5333f7343de2f31c00a
Pranav Molawade <pranavmolawade123@gmail.com>
2024-06-16 17:42:35 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,52 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import numpy as np
+import pickle
+import pandas as pd
+
+# Define the Survival model
+class Survival(BaseModel):
+    Age: float
+    Fare: float
+    Sex: float
+    Pclass: float
+
+# Create the FastAPI app
+app = FastAPI()
+
+# Load the classifier
+pickle_in = open("titanic_model.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define the prediction endpoint
+@app.post('/predict')
+def predict(data: Survival):
+    data = data.dict()
+    Age = data['Age']
+    Fare = data['Fare']
+    Sex = data['Sex']
+    Pclass = data['Pclass']
+    
+    prediction = classifier.predict([[Age, Fare, Sex, Pclass]])
+    
+    if prediction[0] > 0.5:
+        result = 'Survived'
+    else:
+        result = "Died"
+    
+    return {"prediction": result}
+
+# To run the app
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
